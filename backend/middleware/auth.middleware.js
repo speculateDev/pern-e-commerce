bf00653c6ce23c67ce46557af71320252dce83d9
Speculate Dev <sida3li777@gmail.com>
2025-06-11 20:56:44 +0100

    Auth: added protectRoute

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+import { sql } from '../config/db.js';
+
+config();
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized - No token provided',
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized - Invalid token',
+      });
+    }
+
+    const currentUser = await sql`SELECT * FROM users WHERE id = ${decoded.id}`;
+    req.user = currentUser[0];
+
+    next();
+  } catch (error) {
+    console.error('Error in authMiddleware: ', error);
+
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized - No token provided',
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+      });
+    }
+  }
+};
