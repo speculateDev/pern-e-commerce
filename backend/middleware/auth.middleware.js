75f76bd5ef2a39fc6bd13a5e4283922861a5056a
Speculate Dev <sida3li777@gmail.com>
2025-06-11 20:57:14 +0100

    Auth: added isAdmin

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -44,3 +44,41 @@ export const protectRoute = async (req, res, next) => {
     }
   }
 };
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized - No token provided',
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded || !decoded?.id) {
+      return res.status(401).json({
+        success: false,
+        message: 'Not authorized - Invalid token',
+      });
+    }
+
+    const currentUser = await sql`SELECT * FROM users WHERE id = ${decoded.id} LIMIT 1;`;
+
+    if (currentUser[0].role !== 'admin') {
+      return res.status(403).json({
+        success: false,
+        message: 'Unauthorized - You must be an admin',
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.error('Error in isAdmin: ', error);
+    res.status(500).json({
+      success: false,
+    });
+  }
+};
