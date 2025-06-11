2c389946f2db2cff89f37e608ce1e5e15393e61e
Speculate Dev <sida3li777@gmail.com>
2025-06-11 19:08:53 +0100

    Auth: added sql init users

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,16 @@ async function initDB() {
             image VARCHAR(255) NOT NULL,
             category VARCHAR(255) NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
 
+    await sql`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(255) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
 <<<<<<< Updated upstream
 =======
