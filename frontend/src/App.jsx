e6750ac273249ef11348d2e912598bb6e1e39bf5
Speculate Dev <sida3li777@gmail.com>
2025-06-12 18:20:37 +0100

    UI: toaster set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,20 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { useThemeStore } from './stores/useThemeStore';
 import ProductPage from './pages/ProductPage';
 import HomePage from './pages/HomePage';
+import { Toaster } from 'react-hot-toast';
 
 import Navbar from './components/Navbar';
+import { THEMES } from './constants';
 
 function App() {
   const { theme } = useThemeStore();
+
+  // Get toastStyling
+  const currentTheme = THEMES.find((t) => theme === t.name);
+  const toastBackground = currentTheme.colors[0];
+
+  const toastBorder = currentTheme.colors[1];
+  const toastText = currentTheme.colors[2];
 
   return (
     <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
@@ -16,6 +25,16 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
+
+      <Toaster
+        toastOptions={{
+          style: {
+            background: toastBackground,
+            border: `2px solid ${toastBorder}`,
+            color: toastText,
+          },
+        }}
+      />
     </div>
   );
 }
