af30ad4898c22a3e5aa5fa14cce4f2e8b26bb56c
Speculate Dev <sida3li777@gmail.com>
2025-06-12 18:22:59 +0100

    UI: ProductCard added

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,58 @@
+import { EditIcon, Trash2Icon } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { useProductStore } from '../stores/useProductStore';
+
+function ProductCard({ product }) {
+  const { deleteProduct } = useProductStore();
+
+  function handleDelete() {
+    if (!confirm('Are you sure you want to delete?')) return;
+    deleteProduct(product.id);
+  }
+
+  return (
+    <div className="card overflow-hidden bg-base-100 shadow-2xl hover:shadow-2xl transition-shadow duration-300">
+      {/* PRODUCT IMAGE */}
+      <figure className="relative pt-[56.25%]">
+        <img
+          onError={(e) => {
+            e.target.style.display = 'none';
+          }}
+          src={product.image}
+          alt={product.name}
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+      </figure>
+
+      <div className="card-body">
+        {/* PRODUCT INFO */}
+        <h2 className="card-title text-lg font-semibold">{product.name}</h2>
+        <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+
+        {/* CARD ACTIONS */}
+        <div className="card-actions justify-end mt-4">
+          <Link to={`/products/${product.id}`} className="btn btn-sm btn-info btn-outline">
+            <EditIcon className="size-4" />
+          </Link>
+
+<<<<<<< Updated upstream
+          <button
+            className="btn btn-sm btn-error btn-outline"
+            onClick={() => deleteProduct(product.id)}
+          >
+            <Trash2Icon className="size-4" />
+          </button>
+        </div>
+=======
+            <button className="btn btn-sm btn-error btn-outline" onClick={handleDelete}>
+              <Trash2Icon className="size-4" />
+            </button>
+          </div>
+        )}
+>>>>>>> Stashed changes
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
