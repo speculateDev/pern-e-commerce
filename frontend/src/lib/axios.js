fc4fbfc6644e998d35d870a209eb9ce3dbfb6473
Speculate Dev <sida3li777@gmail.com>
2025-06-12 18:21:17 +0100

    ClientSide: axios set up

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,8 @@
+import axiosCreator from 'axios';
+
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api';
+
+export const axios = axiosCreator.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
