36914ab6e8ba392cf29f79926253800148382d81
Speculate Dev <sida3li777@gmail.com>
2025-06-15 11:25:48 +0100

    Cart-Server: updateQuantity added

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -45,3 +45,54 @@ export const addToCart = async (req, res) => {
     });
   }
 };
+
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+
+    if (!user) {
+      return res.status(403).json({
+        message: 'Please log in',
+      });
+    }
+
+    let cartItems = user.cartitems;
+    const existingItem = cartItems.find((item) => item.productId === +productId);
+
+    if (existingItem) {
+      if (+quantity === 0) {
+        cartItems = cartItems.filter((item) => item.productId !== productId);
+
+        const updatedUser = await sql`
+      UPDATE users
+      SET cartitems = ${JSON.stringify(cartItems)}
+      WHERE id = ${user.id} RETURNING *;
+    `;
+
+        return res.json(updatedUser[0].cartitems);
+      }
+
+      existingItem.quantity = +quantity;
+      const updatedUser = await sql`
+        UPDATE users
+        SET cartitems = ${JSON.stringify(cartItems)}
+        WHERE id = ${user.id} RETURNING *;
+      `;
+
+      updatedUser[0].password = null;
+      res.json(updatedUser[0].cartitems);
+    } else {
+      res.status(404).json({
+        message: 'Product not found',
+      });
+    }
+  } catch (error) {
+    console.error('Error in updateQuantity: ', error);
+    res.status(500).json({
+      message: 'Server error',
+      error: error.message,
+    });
+  }
+};
