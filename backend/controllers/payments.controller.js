bb5a04c35d09a06a81e68acb8ff0c94b6eb3bed1
Speculate Dev <sida3li777@gmail.com>
2025-06-18 21:48:00 +0100

    paymentController: checkoutSucccess

--- backend/controllers/payments.controller.js
+++ backend/controllers/payments.controller.js
@@ -60,3 +60,54 @@ export const createCheckoutSession = async (req, res) => {
     });
   }
 };
+
+export const checkoutSuccess = async (req, res) => {
+  try {
+    const { sessionId } = req.body;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+    const products = JSON.parse(session.metadata.products);
+
+    const response = await createOrder(
+      products,
+      session.metadata.userId,
+      sessionId,
+      session.amount_total
+    );
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error('Error in createOrder: ', error);
+    res.status(500).json({
+      message: 'Error processing successful checkout',
+      error: error.message,
+    });
+  }
+};
+
+const createOrder = async (products, userId, sessionId, totalAmount) => {
+  try {
+    const orderId = await sql`
+        INSERT INTO orders (user_id, total_amount, stripe_session_id)
+        VALUES (${userId}, ${totalAmount}, ${sessionId})
+        RETURNING id
+    `;
+
+    products.forEach(async (product) => {
+      await sql`
+        INSERT INTO order_items (order_id, product_id, quantity, unit_price, total_price)
+        VALUES (${orderId[0].id}, ${product.id}, ${product.quantity}, ${product.price}, ${
+        product.price * product.quantity
+      })
+      `;
+    });
+
+    return {
+      message: 'Payment successful, order created.',
+      orderId: orderId[0],
+    };
+  } catch (error) {
+    console.error('Error creating order:', error);
+    throw new Error('Failed to create order');
+  }
+};
