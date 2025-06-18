4572c4669126167d2ab854d6a85ca6602f8c16fc
Speculate Dev <sida3li777@gmail.com>
2025-06-18 20:35:47 +0100

    Fix: resolved conflict on server.js

--- backend/server.js
+++ backend/server.js
@@ -102,42 +102,6 @@ async function initDB() {
             role VARCHAR(50) NOT NULL DEFAULT 'user',
             cartItems JSONB DEFAULT '[]'
         )
-<<<<<<< Updated upstream
-=======
-        `;
-
-    await sql`
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(255) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            role VARCHAR(50) NOT NULL DEFAULT 'user',
-            cartItems JSONB DEFAULT '[]'
-        )
-    `;
-
-    await sql`
-      CREATE TABLE IF NOT EXISTS orders (
-       id SERIAL PRIMARY KEY,
-       user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-       order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       total_amount DECIMAL(10, 2),
-       stripe_session_id VARCHAR(255)
-      )
-    `;
-
-    await sql`
-      CREATE TABLE IF NOT EXISTS order_items(
-        id SERIAL PRIMARY KEY,
-        order_id INTEGER REFERENCES orders(id),
-        product_id INTEGER REFERENCES products(id),
-        quantity INTEGER NOT NULL,
-        unit_price DECIMAL(10, 2),
-        total_price DECIMAL(10, 2)
-      )
->>>>>>> Stashed changes
     `;
 
     await sql`
